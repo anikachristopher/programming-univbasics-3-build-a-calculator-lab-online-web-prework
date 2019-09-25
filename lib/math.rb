362a6518ae8e5efa2f4c97ac4637be532bdceea8
Anika Christopher <anika.mchristopher@gmail.com>
2019-09-25 01:12:41 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
-
+    puts (5,4)
+    sum(5,4)
 end
 
 def subtraction(num1, num2)
-
+  puts (10,5)
+  subtract(10,5)
 end
 
 def division(num1, num2)
-
+  puts (50,2)
+  divide(50,2)
 end
 
 def multiplication(num1, num2)
-
+  puts (4,30)
+  multiply(4,30)
 end
 
 def modulo(num1, num2)
-
+  puts(34,5)
+  modulo(34,5)
 end
 
 def square_root(num)
-
+  puts(81)
+  square_root(81)
 end
